3b1d5ec963b9a0fd3c11109b4b235e646bac7c6e
Kieran Harte <work@kieran.io>
2022-05-27 20:44:12 +0000

    Add new employment

--- src/components/c-about-me/c-about-me.ts
+++ src/components/c-about-me/c-about-me.ts
@@ -4,33 +4,31 @@ import s from 'litsass:./c-about-me.scss'
 
 @customElement('c-about-me')
 export class CAboutMe extends LitElement {
-  static styles = [s]
+	static styles = [s]
 
-  constructor() {
-    super()
-  }
+	constructor() {
+		super()
+	}
 
-  render() {
-    return html`<p>
-        I'm a full-stack web developer currently looking for full-time
-        employment. I recently graduated from Bath Spa University with a First
-        in Creative Computing (Web Technologies).
-      </p>
-      <br />
-      <p>I am passionate about:</p>
-      <ul>
-        <li>Using JS/TS to create native feeling web apps</li>
-        <li>
-          Creating resuable web components (especially with
-          <a href="https://lit.dev">Lit</a>)
-        </li>
-        <li>Self teaching and lifelong learning</li>
-      </ul>`
-  }
+	render() {
+		return html`<p>I have a degree in Creative Computing (Web Technologies) from Bath Spa University and I'm currently building web apps
+	as
+	a front end developer at <a href="https://www.oxbotica.com/">Oxbotica</a> 🚙</p>
+<br />
+<p>I am passionate about:</p>
+<ul>
+	<li>Using web technologies to create native feeling web apps</li>
+	<li>
+		Creating resuable web components (especially with
+		<a href="https://lit.dev">Lit</a>)
+	</li>
+	<li>Self teaching and lifelong learning</li>
+</ul>`
+	}
 }
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'c-about-me': CAboutMe
-  }
+	interface HTMLElementTagNameMap {
+		'c-about-me': CAboutMe
+	}
 }
