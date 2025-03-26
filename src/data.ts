20ef9c2505156a6079f0a07e86abeaf9b4ce4f5e
Kieran Harte <work@kieran.io>
2025-03-26 11:15:59 +0000

    Sort experience from latest to oldest

--- src/data.ts
+++ src/data.ts
@@ -13,12 +13,7 @@ export const projects: Project[] = [
       { src: './images/fractal-c.jpg', alt: 'Coloured Julia Set Fractal' }
     ],
     links: {},
-    tags: ['JavaScript', 'gpu.js'],
-    action: {
-      url: '/fractal',
-      text: 'Open',
-      icon: openInNew
-    }
+    tags: ['JavaScript', 'gpu.js']
   },
   {
     heading: 'QuickShare.dev',
@@ -47,9 +42,9 @@ export const projects: Project[] = [
     links: { github: 'https://github.com/kieran-harte/Glitch-Art' },
     tags: ['Processing', 'Java'],
     action: {
-      url: 'https://drive.google.com/file/d/1CjNhSRMcS68CHHoQ-acVNu2kxpIYOFol/view?usp=sharing',
-      text: 'Download',
-      icon: download
+      url: 'https://github.com/kieran-harte/Glitch-Art',
+      text: 'View Code',
+      icon: openInNew
     }
   },
   {
@@ -59,50 +54,34 @@ export const projects: Project[] = [
     the frontend design and development.`,
     images: [{ src: './images/meeco-c.jpg', alt: 'Meeco Logo' }],
     links: {},
-    tags: ['React Native', 'Node.js', 'MongoDB'],
-    action: {
-      url: 'https://play.google.com/store/apps/details?id=com.kieranharte.meeco',
-      text: 'Download',
-      icon: playStore
-    }
+    tags: ['React Native', 'Node.js', 'MongoDB']
   }
 ]
 
 export const employments: Employment[] = [
   {
-    logo: { src: './images/reyrey.png', alt: 'Reynolds & Reynolds logo' },
-    date: { startEnd: 'Nov 2017 - Apr 2018', duration: '6 months' },
-    role: 'Software Developer',
-    company: 'Reynolds & Reynolds',
-    description: html`This role involved <strong>designing</strong>,
-      <strong>developing</strong>, and <strong>testing</strong> software for
-      their primary software solution, a dealership management system for
-      automotive retailers, written in <strong>COBOL</strong>. It also included
-      finding and <strong>fixing bugs</strong> in existing code, maintaining
-      thorough <strong>documentation</strong>, and participating in
-      <strong>code reviews</strong>.`,
-    tags: ['COBOL']
+    logo: { src: './images/oxa.png', alt: 'Oxa logo' },
+    date: { startEnd: 'October 2021 - Present', duration: '' },
+    role: 'Senior Web Developer',
+    company: 'Oxa',
+    description: html`I work as a Senior Web Developer at Oxa, a company
+      developing software for autonomous vehicles. As part of the Mapping team,
+      I am responsible for <b>designing</b>, <b>developing</b>, and
+      <b>maintaining web based tools</b> to build the maps for autonomous
+      vehicles.`,
+    tags: ['React', 'Typescript', 'WebGL', 'C++']
   },
   {
     logo: { src: './images/bsu-logo.png', alt: 'Bath Spa University Logo' },
-    date: { startEnd: 'Feb - May 2020', duration: '4 months' },
-    role: 'Teaching Assistant',
+    date: { startEnd: 'May - July 2021', duration: '3 months' },
+    role: 'Student Research Associate',
     company: 'Bath Spa University',
-    description: html`I assisted with teaching an introductory web development
-    module, guiding students who were stuck or having technical issues.`,
-    tags: ['HTML5', 'CSS', 'JavaScript']
-  },
-  {
-    logo: { src: './images/bsu-logo.png', alt: 'Bath Spa University Logo' },
-    date: { startEnd: 'Nov - Dec 2020', duration: '1 month' },
-    role: 'Research Assistant',
-    company: 'Bath Spa University',
-    description: html`This work involved researching different technologies for
-      creating cross-platform mobile audio apps. I wrote code using several
-      different technologies: <b>Swift</b>, <b>React Native</b>,
-      <b>C++/JUCE</b>. I then evaluated the developer experience, user
-      experience, and audio performance of each.`,
-    tags: ['SwiftUI', 'React Native', 'C++ (JUCE)']
+    description: html`With the idea and creative direction supplied by the
+      research leader, I coded a new <b>cross-platform mobile</b> audio app to
+      facilitate creative and interactive musical flow. I used the
+      <b>C++</b> framework <b>JUCE</b> and was responsible for
+      <b>testing</b> across a range of mobile devices.`,
+    tags: ['C++', 'JUCE']
   },
   {
     date: { startEnd: 'Mar - July 2021', duration: '5 months' },
@@ -120,27 +99,38 @@ export const employments: Employment[] = [
   },
   {
     logo: { src: './images/bsu-logo.png', alt: 'Bath Spa University Logo' },
-    date: { startEnd: 'May - July 2021', duration: '3 months' },
-    role: 'Student Research Associate',
+    date: { startEnd: 'Nov - Dec 2020', duration: '1 month' },
+    role: 'Research Assistant',
     company: 'Bath Spa University',
-    description: html`With the idea and creative direction supplied by the
-      research leader, I coded a new <b>cross-platform mobile</b> audio app to
-      facilitate creative and interactive musical flow. I used the
-      <b>C++</b> framework <b>JUCE</b> and was responsible for
-      <b>testing</b> across a range of mobile devices.`,
-    tags: ['C++', 'JUCE']
+    description: html`This work involved researching different technologies for
+      creating cross-platform mobile audio apps. I wrote code using several
+      different technologies: <b>Swift</b>, <b>React Native</b>,
+      <b>C++/JUCE</b>. I then evaluated the developer experience, user
+      experience, and audio performance of each.`,
+    tags: ['SwiftUI', 'React Native', 'C++ (JUCE)']
   },
   {
-    logo: { src: './images/oxa.png', alt: 'Oxa logo' },
-    date: { startEnd: 'October 2021 - Present', duration: '' },
-    role: 'Senior Web Developer',
-    company: 'Oxa',
-    description: html`I work as a Senior Web Developer at Oxa, a company
-      developing software for autonomous vehicles. As part of the Mapping team,
-      I am responsible for <b>designing</b>, <b>developing</b>, and
-      <b>maintaining web based tools</b> to build the maps for autonomous
-      vehicles.`,
-    tags: ['React', 'Typescript', 'WebGL', 'C++']
+    logo: { src: './images/bsu-logo.png', alt: 'Bath Spa University Logo' },
+    date: { startEnd: 'Feb - May 2020', duration: '4 months' },
+    role: 'Teaching Assistant',
+    company: 'Bath Spa University',
+    description: html`I assisted with teaching an introductory web development
+    module, guiding students who were stuck or having technical issues.`,
+    tags: ['HTML5', 'CSS', 'JavaScript']
+  },
+  {
+    logo: { src: './images/reyrey.png', alt: 'Reynolds & Reynolds logo' },
+    date: { startEnd: 'Nov 2017 - Apr 2018', duration: '6 months' },
+    role: 'Software Developer',
+    company: 'Reynolds & Reynolds',
+    description: html`This role involved <strong>designing</strong>,
+      <strong>developing</strong>, and <strong>testing</strong> software for
+      their primary software solution, a dealership management system for
+      automotive retailers, written in <strong>COBOL</strong>. It also included
+      finding and <strong>fixing bugs</strong> in existing code, maintaining
+      thorough <strong>documentation</strong>, and participating in
+      <strong>code reviews</strong>.`,
+    tags: ['COBOL']
   }
 ]
 
